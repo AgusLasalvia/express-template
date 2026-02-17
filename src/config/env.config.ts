d15172b053f2e56443c77a3e812faf1c1000ef9c
Agustin Lasalvia <64497665+AgusLasalvia@users.noreply.github.com>
2026-02-17 12:39:18 -0300

    Uncomment database configuration in env.config.ts
    Uncomment database configuration properties for clarity.

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -4,13 +4,14 @@ config()
 
 export const SYSTEM = {
     PORT: (process.env.PORT || 3000) as number,
-    // DATABASE_NAME: process.env.DATABASE_NAME as string,
-    // DATABASE_USER: process.env.DATABASE_USER as string,
-    // DATABASE_PASSWORD: process.env.DATABASE_PASSWORD as string
+
 }
 
 export const DATABASE = {
     DATABASE_URL: process.env.DATABASE_URL as string
+        // DATABASE_NAME: process.env.DATABASE_NAME as string,
+    // DATABASE_USER: process.env.DATABASE_USER as string,
+    // DATABASE_PASSWORD: process.env.DATABASE_PASSWORD as string
 }
 
 export const JWT = {
